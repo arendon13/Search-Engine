2c6bf3dcbb3bf12fc79ce4139404333ef505937f
BenjiEvans <benjievans25@yahoo.com>
2016-02-08 19:04:14 -0800

    Created CrallerApp as entry point for Crawlling program

---
+++ src/main/java/edu/csula/cs454/crawler/CrawlerApp.java
@@ -0,0 +1,9 @@
+package edu.csula.cs454.crawler;
+
+public class CrawlerApp {
+	 public static void main(String[] args){
+			
+		 CrawlerController controller = CrawlerController.getInstance();
+		  System.out.println("Hello from Crawler Controller");  
+	  }
+}
--- src/main/java/edu/csula/cs454/crawler/CrawlerController.java
+++ src/main/java/edu/csula/cs454/crawler/CrawlerController.java
@@ -1,12 +1,13 @@
 package edu.csula.cs454.crawler;
 
+//This will be a Singleton instance to avoid multithreaded confusion
 public class CrawlerController {
-	
-	  public static void main(String[] args){
-		
-		  System.out.println("Hello from Crawler Controller");
-		  
-		  
-		  
-	  }
+	 private static CrawlerController instance = null;
+	 private CrawlerController(){}
+	 public static CrawlerController getInstance(){
+		 if(instance == null) {
+	         instance = new CrawlerController();
+	      }
+	      return instance;		 
+	 } 
 }
