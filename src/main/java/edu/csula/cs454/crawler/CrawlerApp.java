ee47c6db4ef59f5103a833d552bca603791185b8
BenjiEvans <benjievans25@yahoo.com>
2016-02-09 11:07:21 -0800

    Completed Crawler driver. Will need to be revised to support command line arguments

--- src/main/java/edu/csula/cs454/crawler/CrawlerApp.java
+++ src/main/java/edu/csula/cs454/crawler/CrawlerApp.java
@@ -2,8 +2,12 @@ package edu.csula.cs454.crawler;
 
 public class CrawlerApp {
 	 public static void main(String[] args){
-			
+		 String seed = "";
+		 int depth = 2;
 		 CrawlerController controller = CrawlerController.getInstance();
-		  System.out.println("Hello from Crawler Controller");  
+		 controller.addSeed(seed);
+		 controller.setStorageFolder("CrawlerStorge");
+		 controller.setDepth(depth);
+		 controller.start(); 
 	  }
 }
--- src/main/java/edu/csula/cs454/crawler/CrawlerController.java
+++ src/main/java/edu/csula/cs454/crawler/CrawlerController.java
@@ -3,11 +3,28 @@ package edu.csula.cs454.crawler;
 //This will be a Singleton instance to avoid multithreaded confusion
 public class CrawlerController {
 	 private static CrawlerController instance = null;
+	 MyCrawler crawler;
+	 private String storageFolder;
 	 private CrawlerController(){}
 	 public static CrawlerController getInstance(){
 		 if(instance == null) {
 	         instance = new CrawlerController();
 	      }
 	      return instance;		 
-	 } 
+	 }
+	public void addSeed(String seed) {
+		// TODO add see to the crawler
+		  
+	}
+	public void setStorageFolder(String folder) {
+		storageFolder = folder;
+	}
+	
+	public void setDepth(int depth){
+		// TODO add depth to crawler 
+	}
+	
+	public void start (){
+		//TODO start crawling and extracting meta data 
+	}
 }
