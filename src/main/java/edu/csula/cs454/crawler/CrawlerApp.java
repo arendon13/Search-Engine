41412d878471179c4c7716fd8918faf0e9d78ba3
BenjiEvans <benjievans25@yahoo.com>
2016-02-12 14:00:22 -0800

    Updated crawler app so that it handles command line arguments. However have not been able to pass command line arguments from gradle :(

--- src/main/java/edu/csula/cs454/crawler/CrawlerApp.java
+++ src/main/java/edu/csula/cs454/crawler/CrawlerApp.java
@@ -4,14 +4,27 @@ public class CrawlerApp {
 	 public static void main(String[] args){
 		 //String seed = "http://www.calstatela.edu/";
 		 //String seed = "http://maplestory.nexon.net/";
-		 String seed = "http://google.com";
-		 int depth = 2;
-		 CrawlerController controller = CrawlerController.getInstance();
-		 controller.setStorageFolder("CrawlerStorge");
-		 controller.setDepth(depth);
-		 controller.addSeed(seed);
-		 controller.setMetaDataStorageDatabase("CrawledData");
-		 controller.enableExtraction(true);
-		 controller.start(); 
+		 //-d <depth> -u <url> -e
+		 args = new String[]{"-d","2","-u","http://www.calstatela.edu/"};
+		 if(args.length > 3 && args[0] == "-d" && args[2] == "-u")
+		 {
+			 String seed = args[3];
+			 int depth = Integer.parseInt(args[1]);
+			 CrawlerController controller = CrawlerController.getInstance();
+			 controller.setStorageFolder("CrawlerStorge");
+			 controller.setDepth(depth);
+			 controller.addSeed(seed);
+			 controller.setMetaDataStorageDatabase("CrawledData");
+			 if(args.length == 5 && args[4] == "-e")controller.enableExtraction(true);
+			 else controller.enableExtraction(false);			 
+			 controller.start(); 			 
+			 
+		 }else{
+			 System.out.println("Not Enough Command Line arguments. Or incorrect argumuments passed in...");
+			 for(String s : args){
+				 System.out.print(s);
+			 }
+		 }
+		
 	  }
 }
