671acd5a6dc54b88fb6843c0efc03d99280c1e77
Adrian Rendon <arendon@Adrians-MacBook-Pro-2.local>
2016-02-12 23:33:40 -0800

    Added Recursive Crawling

--- src/main/java/edu/csula/cs454/crawler/WebCrawler.java
+++ src/main/java/edu/csula/cs454/crawler/WebCrawler.java
@@ -1,5 +1,6 @@
 package edu.csula.cs454.crawler;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Stack;
 
@@ -9,6 +10,7 @@ import org.jsoup.select.Elements;
 public class WebCrawler {
 	
 	private ArrayList<String> seeds;
+	private ArrayList<String> visited = new ArrayList<String>();
 	private String storageFolder;
 	private int crawlDepth;
 	private Stack<Document> docs;
@@ -30,32 +32,82 @@ public class WebCrawler {
 	}
 	
 	public void crawl(){
-		/*TODO 
-		 implement so that it recursively crawls (without circulation)
-		 (breadth first) till the depth specified
-		*
-		*/
+		recursCrawl(0, crawlDepth, seeds);
 		
-		for(String seed: seeds)
-		{
-			try{
-				//get document 
-				Document doc = Jsoup.connect(seed).get();
-				//extract all links for the document
-				ArrayList<String> links = new ArrayList<String>();
+		visited = null;
+		
+//		for(String seed: seeds)
+//		{
+//			try{
+//				//get document 
+//				Document doc = Jsoup.connect(seed).get();
+//				//extract all links for the document
+//				ArrayList<String> links = new ArrayList<String>();
+//				Elements elts = doc.select("a");
+//				//System.out.println("Links found: "+elts.size());
+//				for(int i = 0, length = elts.size(); i < length;i++)
+//				{
+//					links.add(elts.get(i).absUrl("href"));
+//				}	
+//				//add document to list of documents crawled
+//				docs.push(doc);				
+//				
+//			}catch(Exception e){
+//				System.out.println("Something went wrong!!!");
+//				System.exit(0);
+//			}			
+//		}		
+	}
+	
+	private void recursCrawl(int curDepth, int maxDepth, ArrayList<String> links) {
+		ArrayList<String> childLinks = new ArrayList<String>();
+		
+		for(String i: links){
+			if(i.trim().length() == 0 || isVisited(i)){
+				continue;
+			}
+			addToVisited(i);
+			Document doc;
+			System.out.println(curDepth + ": Connecting to..." + i);
+			try {
+				doc = Jsoup.connect(i).get();
+
 				Elements elts = doc.select("a");
 				//System.out.println("Links found: "+elts.size());
-				for(int i = 0, length = elts.size(); i < length;i++)
+				for(int j = 0, length = elts.size(); j < length; j++)
 				{
-					links.add(elts.get(i).absUrl("href"));
-				}	
+					childLinks.add(elts.get(j).absUrl("href"));
+//					System.out.println(elts.get(j).absUrl("href"));
+				}
 				//add document to list of documents crawled
-				docs.push(doc);				
-				
-			}catch(Exception e){
-				System.out.println("Something went wrong!!!");
-				System.exit(0);
-			}			
-		}		
+				docs.push(doc);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+//				e.printStackTrace();
+			}
+		}
+		
+		if(curDepth < maxDepth){
+			recursCrawl(curDepth + 1, maxDepth, childLinks);
+		}
+		else{
+			return;
+		}
+		
+	}
+	
+	private boolean isVisited(String link){
+		
+		for(String i: visited){
+			if(i.equalsIgnoreCase(link)){
+				return true;
+			}
+		}
+		
+		return false;
+	}
+	
+	private void addToVisited(String link){
+		visited.add(link);
 	}
 }
