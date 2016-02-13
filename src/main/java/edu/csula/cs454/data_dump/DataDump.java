0bacc6f483fbd12909def15fa6ce36916ee75cc2
easauceda <easauceda@gmail.com>
2016-02-13 10:30:08 -0800

    Removed failed morphia fields

--- src/main/java/edu/csula/cs454/data_dump/DataDump.java
+++ src/main/java/edu/csula/cs454/data_dump/DataDump.java
@@ -3,11 +3,7 @@ package edu.csula.cs454.data_dump;
 import com.mongodb.MongoClient;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
-import edu.csula.cs454.crawler.DocumentMetadata;
-import org.bson.BSON;
 import org.json.*;
-import org.mongodb.morphia.*;
-import org.mongodb.morphia.query.Query;
 
 import java.io.FileWriter;
 import java.io.IOException;
@@ -20,22 +16,13 @@ public class DataDump {
 	private static MongoClient mongo;
 	private static MongoCollection collection;
 	private static int iterator = 1;
-	private static Datastore dataStore = null;
 
 	public static void main(String[] args) {
-
-		Morphia morphia = new Morphia();
-		morphia.map(DocumentMetadata.class);
-
 		mongo = new MongoClient();
-		dataStore = morphia.createDatastore(mongo, "CrawledData");
-
-		//Data dump code goes here lol as if you couldn't tell
 		db = mongo.getDatabase("CrawledData");
 		collection = db.getCollection("DocumentMetadata");
 
 		JSONObject dump = new JSONObject();
-
 
 		for (Object val : collection.find().projection(fields(exclude("_id")))) {
 			dump.append(String.valueOf(iterator), val);
