c695f3a140c8f4c795f7ec37114fc1cedd3843d1
easauceda <easauceda@gmail.com>
2016-03-12 03:34:01 -0800

    updating website, lots of new features. Search by term, result list, formatting

--- website/app.js
+++ website/app.js
@@ -1,13 +1,22 @@
 var express = require('express');
 var path = require('path');
 var app = express();
+var MongoClient = require('mongodb').MongoClient;
+var collection = require('mongodb').Collection;
+var db;
+var url = 'mongodb://localhost:27017/CrawledData'
+var Promise = require('bluebird');
+
+Promise.promisifyAll(MongoClient);
+Promise.promisifyAll(collection.prototype);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('*', function(req, res){
-	res.sendFile(path.resolve('index.html'));
-});
-
-app.listen('3000', function() {
-	console.log('Server started, port 3000');
+MongoClient.connect(url, function(err, database) {
+	if (err) throw err;
+	app.set('mongo', database);
+	require('./routes')(app);
+	app.listen(3000, function(){
+		console.log('Listening on port 3000');
+	});
 });
